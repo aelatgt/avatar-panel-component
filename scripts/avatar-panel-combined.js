a9ac36c4696fa8e4c0f9a476cc8a38f03fc9bb22
Pujith Kachana <72110347+pkachana3@users.noreply.github.com>
2021-04-27 14:16:54 -0400

    Update avatar-panel-combined.js

--- scripts/avatar-panel-combined.js
+++ scripts/avatar-panel-combined.js
@@ -1,7 +1,7 @@
 /**
+ * Taken from Vincent's hover shape component
  * Sets up an entity with the holdable button functionality so that it can alter
  * scale on cursor movement.
- * Taken from Vincent's hover shape component
  */
 
  AFRAME.registerComponent('drag-scale', {
@@ -52,7 +52,8 @@
 		} else if (this.el.object3D.scale.y < this.scaleMin.y) {
 			  this.el.object3D.scale.y = this.scaleMin.y;
 			  this.el.object3D.scale.x = this.scaleMin.x;
-			  this.el.object3D.scale.z = this.scaleMin.z;
+			  this.el.object3D.scale.z = this.scaleMin.z; 
+			  this.el.object3D.visible = false; //makes the component invisible if it's scaled below min threshold
 		}
 		// Store cursor position for next frame.
 		this.prevPosition.copy(this.dragCursor.position)
@@ -65,17 +66,28 @@
    * Component to allow a hubs entity to adjust rotation and position depending on avatar.
    * Also adds a small, constant hover to the entity
    */
-  
+
   AFRAME.registerComponent('panel-shape', {
+	  init: function () {
+		this.totalTurns = 0;
+		this.direction = 0;
+	  },
+	  
 	  tick: function () {
+		
 		  var el = this.el;
 		  let avatarPOV = document.querySelector("#avatar-pov-node")
 		  var y = avatarPOV.object3D.position.y - 0.5;
 		  el.setAttribute('position', el.object3D.position.x + ', ' + (y + Math.sin(Date.now() / 500) * .05) + ', ' + el.object3D.position.z);
 
+		  //Sets the text on the component
+		  this.el.setAttribute('text', 'value', "You have turned " +this.totalTurns+ " times");
+		  this.el.setAttribute('text', 'align', 'center');
+
 		  //Get the avatar POV's angle to see where it's facing
 		  let pov = document.querySelector("#avatar-pov-node")
 		  var angle = pov.rotationObj.y + 135
+
 		
 		  //Change the entity's position and rotation depending on the angle
 			if (angle >= 0 && angle < 90) {
@@ -84,6 +96,12 @@
 				el.object3D.rotation.y = 3 * Math.PI/2;
 				el.object3D.rotation.z = -Math.PI/4;
 				el.object3D.rotation.x = -Math.PI/4;
+
+				if (this.direction != 0) {
+					this.totalTurns++;
+					el.object3D.visible = true;
+				}
+				this.direction = 0;
 			}
 			else if (angle >= 90 && angle < 180) {
 				el.object3D.position.x = 0;
@@ -91,6 +109,12 @@
 				el.object3D.rotation.y = 0;
 				el.object3D.rotation.x = -Math.PI/4;
 				el.object3D.rotation.z = 0;
+
+				if (this.direction != 1) {
+					this.totalTurns++;
+					el.object3D.visible = true;
+				}
+				this.direction = 1;
 			}
 			else if (angle >= 180 && angle < 270) {
 				el.object3D.position.x = -0.5;
@@ -98,15 +122,28 @@
 				el.object3D.rotation.y = Math.PI/2;
 				el.object3D.rotation.z = Math.PI/4;
 				el.object3D.rotation.x = -Math.PI/4;
+
+				if (this.direction != 2) {
+					this.totalTurns++;
+					el.object3D.visible = true;
+				}
+				this.direction = 2;
 			} else {
 				el.object3D.position.x = 0;
 				el.object3D.position.z = 0.5;
 				el.object3D.rotation.y = Math.PI; 
 				el.object3D.rotation.z = 0;
 				el.object3D.rotation.x = Math.PI/4;
+
+				if (this.direction != 3) {
+					this.totalTurns++;
+					el.object3D.visible = true;
+				}
+				this.direction = 3;
 			}
 	  },
   });
+
   
   //Query assets in order to setup template
   const assets = document.querySelector("a-assets");
